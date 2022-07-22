38b2bd4c34b6952c72485f1b919d6ab5ad71cd68
dipalak1234 <109249554+dipalak1234@users.noreply.github.com>
2022-07-23 00:29:50 +0530

    Add files via upload

---
+++ assignment 4.c
@@ -0,0 +1,127 @@
+//Assignment 4 iterative Control Statement
+/*#include<stdio.h>//question 1
+int main()
+{
+	int a=1;
+	while(a<=5)
+	{
+		printf("MySirG 5\n");
+		a++;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 2
+int main()
+{
+	int a=1;
+	while(a<=10)
+	{
+		printf("%d\n",a);
+		a++;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 3
+int main()
+{
+	int a=10;
+	while(a>=1)
+	{
+		printf("%d\n",a);
+		a--;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 4
+int main()
+{
+	int a=1,b=1;
+	while(a<=10)
+	{
+		printf("%d\n",b);
+		a++;
+		b+=2;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 5
+int main()
+{
+	int a=1,b=1;
+	while(a<=9)
+	{	
+		a++;
+		b+=2;
+	}
+	while(a>=1)
+	{
+		printf("%d\n",b);
+		b-=2;
+		a--;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 6
+int main()
+{
+	int a=1,b=2;
+	while(a<=10)
+	{
+		printf("%d\n",b);
+		a++;
+		b+=2;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 7
+int main()
+{
+	int a=1,b=2;
+	while(a<=9)
+	{	
+		a++;
+		b+=2;
+	}
+	while(a>=1)
+	{
+		printf("%d\n",b);
+		b-=2;
+		a--;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 8
+int main()
+{
+	int a=1,b=1;
+	while(a<=10)
+	{
+		printf("%d\n",b);
+		a++;
+		b=a*a;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 9
+int main()
+{
+	int a=1,b=1;
+	while(a<=10)
+	{
+		printf("%d\n",b);
+		a++;
+		b=a*a*a;
+	}
+	return 0;
+}*/
+/*#include<stdio.h>//question 10
+int main()
+{
+	int a=1,b;
+	while(a<=15)
+	{
+		printf("%d * 5 = %d\n",a,b=a*5);
+		a++;
+	}
+	return 0;
+}*/
